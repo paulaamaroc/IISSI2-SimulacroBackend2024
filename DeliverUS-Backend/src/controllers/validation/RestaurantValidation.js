1ff382194159a94c53cf6942285a93c609a7f5f9
paulaamaroc <pauamacal@alum.us.es>
2025-03-19 13:29:24 +0100

    first commit

---
+++ DeliverUS-Backend/src/controllers/validation/RestaurantValidation.js
@@ -0,0 +1,75 @@
+import { check } from 'express-validator'
+import { checkFileIsImage, checkFileMaxSize } from './FileValidationHelper.js'
+import { Restaurant } from '#root/src/models/models.js'
+const maxFileSize = 2000000 // around 2Mb
+
+const checkRestaurantPromoted = async (promoted, { req }) => {
+  try {
+    if (promoted) {
+      const restaurantPromoted = await Restaurant.findOne({
+        where:
+        {
+          promoted: true
+        }
+      })
+      if (restaurantPromoted) {
+        return Promise.reject(new Error('There is a restaurant already promoted'))
+      }
+    }
+    return Promise.resolve()
+  } catch (err) {
+    return Promise.reject(new Error(err))
+  }
+}
+
+const create = [
+  check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
+  check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+  check('restaurantCategoryId').exists({ checkNull: true }).isInt({ min: 1 }).toInt(),
+  check('userId').not().exists(),
+  check('promoted').optional().isBoolean().custom(checkRestaurantPromoted),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileIsImage(req, 'heroImage')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'heroImage', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  check('logo').custom((value, { req }) => {
+    return checkFileIsImage(req, 'logo')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('logo').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'logo', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
+]
+const update = [
+  check('name').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('description').optional({ nullable: true, checkFalsy: true }).isString().trim(),
+  check('address').exists().isString().isLength({ min: 1, max: 255 }).trim(),
+  check('postalCode').exists().isString().isLength({ min: 1, max: 255 }),
+  check('url').optional({ nullable: true, checkFalsy: true }).isString().isURL().trim(),
+  check('shippingCosts').exists().isFloat({ min: 0 }).toFloat(),
+  check('email').optional({ nullable: true, checkFalsy: true }).isString().isEmail().trim(),
+  check('phone').optional({ nullable: true, checkFalsy: true }).isString().isLength({ min: 1, max: 255 }).trim(),
+  check('restaurantCategoryId').exists({ checkNull: true }).isInt({ min: 1 }).toInt(),
+  check('userId').not().exists(),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileIsImage(req, 'heroImage')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('heroImage').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'heroImage', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB'),
+  check('logo').custom((value, { req }) => {
+    return checkFileIsImage(req, 'logo')
+  }).withMessage('Please upload an image with format (jpeg, png).'),
+  check('logo').custom((value, { req }) => {
+    return checkFileMaxSize(req, 'logo', maxFileSize)
+  }).withMessage('Maximum file size of ' + maxFileSize / 1000000 + 'MB')
+]
+
+export { create, update }
